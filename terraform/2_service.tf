b0fe7648a6a0f1e75e7b23952ae9f2d38353309a
Bh-an <bhan.16070@gmail.com>
2026-03-27 01:43:40 +0530

    feat(terraform): add ssm-backed ami contract

--- terraform/2_service.tf
+++ terraform/2_service.tf
@@ -1,17 +1,18 @@
 module "service" {
   source = "./modules/service-host"
 
-  platform             = var.platform
-  environment          = var.environment
-  vpc_id               = module.network.vpc_id
-  subnet_id            = module.network.public_subnet_ids[0]
-  availability_zone    = var.availability_zones[0]
-  instance_type        = var.instance_type
-  key_pair_name        = var.key_pair_name
-  docker_image         = var.docker_image
-  ami_name_prefix      = var.ami_name_prefix
-  root_volume_size_gib = var.root_volume_size_gib
-  data_volume_size_gib = var.data_volume_size_gib
-  enable_elastic_ip    = var.enable_elastic_ip
-  ingress_rules        = var.ingress_rules
+  platform               = var.platform
+  environment            = var.environment
+  vpc_id                 = module.network.vpc_id
+  subnet_id              = module.network.public_subnet_ids[0]
+  availability_zone      = var.availability_zones[0]
+  instance_type          = var.instance_type
+  key_pair_name          = var.key_pair_name
+  docker_image           = var.docker_image
+  ami_name_prefix        = var.ami_name_prefix
+  ami_ssm_parameter_name = var.ami_ssm_parameter_name
+  root_volume_size_gib   = var.root_volume_size_gib
+  data_volume_size_gib   = var.data_volume_size_gib
+  enable_elastic_ip      = var.enable_elastic_ip
+  ingress_rules          = var.ingress_rules
 }
